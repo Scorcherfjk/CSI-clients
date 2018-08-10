89c0305b6ea639054a93c0b5a3a8624d16b568b6
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-10 17:05:41 -0500

    anadiendo el modulo de actualizacion

---
+++ update.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "<br>".$idresponsable = $_POST['idresponsable'];
+
+echo "<br>".$idcontacto = $_POST['idcontacto'];
+
+echo "<br>".$registro = $_POST['registro'];
+echo "<br>".$aceptacion = $_POST['aceptacion'];
+echo "<br>".$visita = $_POST['visita'];
+echo "<br>".$consultas = $_POST['consultas'];
+echo "<br>".$respuestas = $_POST['respuestas'];
+echo "<br>".$oferta = $_POST['oferta'];
+echo "<br>".$decision = $_POST['decision'];
+
+echo "<br>".$enviado = $_POST['enviado'];
+echo "<br>".$cotizacion = $_POST['cotizacion'];
+echo "<br>".$monto = $_POST['monto'];
+echo "<br>".$comentario = $_POST['comentario'];
+
+
+?>
