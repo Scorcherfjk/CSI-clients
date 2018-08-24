3642b8bad13e19a3df389ed49ba5b4368a92e19a
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-24 17:30:34 -0500

    deberia funcionar... aun sin probarlo

--- update.php
+++ update.php
@@ -1,21 +1,69 @@
 <?php
 
-echo "<br>".$idresponsable = $_POST['idresponsable'];
+// incluye la variable de la conexion a la base de datos
+require("conexion.php"); 
 
-echo "<br>".$idcontacto = $_POST['idcontacto'];
-
-echo "<br>".$registro = $_POST['registro'];
-echo "<br>".$aceptacion = $_POST['aceptacion'];
-echo "<br>".$visita = $_POST['visita'];
-echo "<br>".$consultas = $_POST['consultas'];
-echo "<br>".$respuestas = $_POST['respuestas'];
-echo "<br>".$oferta = $_POST['oferta'];
-echo "<br>".$decision = $_POST['decision'];
-
-echo "<br>".$enviado = $_POST['enviado'];
-echo "<br>".$cotizacion = $_POST['cotizacion'];
-echo "<br>".$monto = $_POST['monto'];
-echo "<br>".$comentario = $_POST['comentario'];
+// los id
+$idproyecto = $_POST['idproyecto'];
+$idresponsable = $_POST['idresponsable'];
+$idcontacto = $_POST['idcontacto'];
 
 
+// las fechas
+$registro = $_POST['registro'];
+$aceptacion = $_POST['aceptacion'];
+$visita = $_POST['visita'];
+$consultas = $_POST['consultas'];
+$respuestas = $_POST['respuestas'];
+$oferta = $_POST['oferta'];
+$decision = $_POST['decision'];
+
+// los datos extra
+$enviado = $_POST['enviado'];
+$cotizacion = $_POST['cotizacion'];
+$monto = $_POST['monto'];
+$comentario = $_POST['comentario'];
+
+// Query
+$option = 
+"UPDATE 
+    proyecto
+SET 
+    responsable_idresponsable = '$idresponsable',
+    contacto_idcontacto = '$idcontacto',
+    enviado = '$enviado',
+    cotizacion = '$cotizacion',
+    monto = '$monto',
+    comnetario = '$comentario'
+WHERE
+    idproyecto = '$idproyecto'";
+
+// Ejecutando el SQL
+$optionready = mysqli_query($conexion,$option);
+mysqli_commit($conexion);
+mysqli_free_result($optionready);
+
+// Query2
+$option2 = 
+"UPDATE 
+    fecha
+SET 
+    responsable_idresponsable = '$idresponsable',
+    contacto_idcontacto = '$idcontacto',
+    registro = '$registro'
+    aceptacion = '$aceptacion
+    visita = '$visita'
+    consultas = '$consultas'
+    respuestas = '$respuestas'
+    oferta = '$oferta'
+    desicion = '$decision'
+WHERE
+    proyecto_idproyecto = '$idproyecto'";
+
+// Ejecutando el SQL 2
+$optionready2 = mysqli_query($conexion,$option2);
+mysqli_commit($conexion);
+mysqli_free_result($optionready2);
+mysqli_close($conexion);
+header("location:http://localhost:8080/FTMetrics/CSI-clients/add_proyect.php");
 ?>
