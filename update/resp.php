8349974ef456fecd672b59883d75a2e29f99655b
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-24 11:42:10 -0500

    arreglo update

--- update/resp.php
+++ update/resp.php
@@ -4,12 +4,13 @@ require("conexion.php"); // incluye la variable de la conexion a la base de dato
 
 $nombre = $_POST['name'];
 $apellido = $_POST['lastname'];
+$iniciales = substr($nombre,0,1).substr($apellido,0,1);
 $email = $_POST['email'];
 $telefono = $_POST['tel'];
 $celular = $_POST['cel'];
 $cliente = $_POST['cliente'];
 
-$option = "INSERT INTO responsable (idresponsable, nombre,apellido,email,telefono,celular) values ('','$nombre','$apellido','$email','$telefono','$celular')";
+$option = "INSERT INTO responsable (idresponsable,iniciales,nombre,apellido,email,telefono,celular) values ('','$iniciales','$nombre','$apellido','$email','$telefono','$celular')";
 
 $optionready = mysqli_query($conexion,$option);
 mysqli_commit($conexion);
